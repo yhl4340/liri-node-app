2b97209781ad8f084624a407fb0c9bfa0bc3a4ee
Lyly <yhl4340@gmail.com>
2019-02-20 20:47:57 -0800

    trying to set up spotify

--- liri.js
+++ liri.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
+//
 
-//require the axios package
+require('dotenv').config();
+var keys = require("./keys.js");
 var axios = require('axios');
 var fs = require('fs');
 var moment = require('moment');
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
-// exports.spotify = {
-//     id: process.env.SPOTIFY_ID,
-//     secret: process.env.SPOTIFY_SECRET
-// }
 
 // user input from the command line. 
 var nodeArgv = process.argv;
+console.log(nodeArgv);
 // command line arg starts at 2.
 
 // var command = nodeArgv.slice(2);
@@ -45,36 +44,55 @@ var queryInput = '';
 
 
 
-var bandUrl = 
-"https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp#";
+// var bandUrl = 
+// "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp#";
 
-axios.get(bandUrl).then(function( res){
+// axios.get(bandUrl).then(function( res){
     
-    var jData = res.data;
+//     var jData = res.data;
     
     
-    for (var i = 0; i < jData.length; i ++){
-        var date = jData[i].datetime;
-        var momentDate = moment(date).format('MMM Do yy, h:mm:ss a');
+//     for (var i = 0; i < jData.length; i ++){
+//         var date = jData[i].datetime;
+//         var momentDate = moment(date).format('MMM Do yy, h:mm:ss a');
         
-        console.log('===============================');
-        console.log('Name of the venue: ' + jData[i].venue.name);
-        console.log('City of the venue: ' + jData[i].venue.city); 
-        console.log('Time of the venue: ' + momentDate);
-        console.log('---------------------------------'); 
-    }
+//         console.log('===============================');
+//         console.log('Name of the venue: ' + jData[i].venue.name);
+//         console.log('City of the venue: ' + jData[i].venue.city); 
+//         console.log('Time of the venue: ' + momentDate);
+//         console.log('---------------------------------'); 
+//     }
    
-    })
-    .catch(function(err){
-        if(err){
-            console.log(err.res.data);
-            console.log(err.res.status);
-            console.log(err.res.headers);
-        }else if (err.request){
-            console.log(err.request);
-        }else{
-            console.log("Error", err.message);
-        }
-        console.log(err.config);
-    });
+//     })
+//     .catch(function(err){
+//         if(err){
+//             console.log(err.res.data);
+//             console.log(err.res.status);
+//             console.log(err.res.headers);
+//         }else if (err.request){
+//             console.log(err.request);
+//         }else{
+//             console.log("Error", err.message);
+//         }
+//         console.log(err.config);
+//     });
 
+
+    //---------------SPOTIFY------------------
+var base = 'https://api.spotify.com/v1/artists/99471d95db844a81a3b63d3b27d43b5e/'+ input;
+
+
+// sample for album
+//  GET https://api.spotify.com/v1/albums/
+
+spotify.search({
+    type:'track',
+    query: 'all the small things',
+    limit:15
+},function(err,data){
+    if(err){
+        return console.log('Error occured: ' + err)
+    }
+    console.log(data);
+
+});
